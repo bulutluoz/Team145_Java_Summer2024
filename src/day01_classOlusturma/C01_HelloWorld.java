dd7adcf3bbdf2fafa2165165d6e031ef8229aa68
bulutluoz <abbulutluoz@gmail.com>
2024-07-01 22:00:30 +0200

    day01 sonu

---
+++ src/day01_classOlusturma/C01_HelloWorld.java
@@ -0,0 +1,37 @@
+package day01_classOlusturma;
+
+public class C01_HelloWorld {
+
+
+    public static void main(String[] args) {
+
+        // buraya yazilan kodlari Java compile etmez
+        // yani bir kod olarak kabul etmez
+        // bu sekilde yazilan notlar, sonraki kullanimlarimiz
+        // veya farkli insanlarin kullaniminda kodu kolayca anlamalari icindir
+
+        /*
+            bu
+            sekilde
+            istedigimiz kadar
+            yorum satiri yazabiliriz
+            her satira // koymamiza gerek kalmaz
+         */
+
+
+        System.out.println("Hello World");
+
+        System.out.println(  "Merhaba Java" );
+
+
+        System.out.println
+                (
+                        "noktali virgule kadar " +
+                "kod tek parca kabul edilir"
+        )
+        ;
+        // kod satiri yazilmaya basladiktan sonra ;'e kadar olan kisimn tek satir kabul edilir
+
+    }
+
+}
---
+++ src/day01_classOlusturma/C02_VariableKullanimi.java
@@ -0,0 +1,28 @@
+package day01_classOlusturma;
+
+public class C02_VariableKullanimi {
+
+    public static void main(String[] args) {
+
+
+        int sayi  = 20 ;
+
+
+
+
+
+        System.out.println(sayi); // 20
+        // sout icerisinde cift tirnak kullanildiginda
+        // "" arasindaki metin oldugu gibi yazdirilir
+        // "" yoksa, java yazdiracagi seyin
+        // bir variable'in degeri oldugunu bilir
+
+
+        System.out.println("sayi"); // sayi
+
+        sayi = sayi + 5 ;
+
+        System.out.println(sayi); // 25
+
+    }
+}
