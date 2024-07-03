cdf30710454491cc36775ce6417bb850deb67647
bulutluoz <abbulutluoz@gmail.com>
2024-07-03 20:46:15 +0200

    day03 sonu

---
+++ src/day03_scanner_dataCasting/C05_BilgileriDegistirme_Swap.java
@@ -0,0 +1,28 @@
+package day03_scanner_dataCasting;
+
+import java.util.Scanner;
+
+public class C05_BilgileriDegistirme_Swap {
+
+    public static void main(String[] args) {
+
+        // Soru 7 (Interview)- Kullanicidan iki sayi alip ikisinin degerlerini degistirin(swap).
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen 1.tamsayiyi giriniz...");
+
+        int sayi1 = scanner.nextInt();
+
+        System.out.println("Lutfen 2.tamsayiyi giriniz...");
+
+        int sayi2 = scanner.nextInt();
+
+        sayi1 = sayi2;
+        sayi2 = sayi1 ;
+
+
+        System.out.println("Sayi1'in yeni degeri : " + sayi1);
+        System.out.println("Sayi2'nin yeni degeri : " + sayi2);
+    }
+}
