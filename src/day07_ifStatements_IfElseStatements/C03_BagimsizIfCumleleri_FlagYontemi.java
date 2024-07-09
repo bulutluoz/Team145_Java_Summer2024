20aab47715a856f0e12f8070ca87f24af1e247d1
bulutluoz <abbulutluoz@gmail.com>
2024-07-09 20:26:04 +0200

    7.gun sonu

--- src/day07_ifStatements_IfElseStatements/C03_BagimsizIfCumleleri_FlagYontemi.java
+++ src/day07_ifStatements_IfElseStatements/C03_BagimsizIfCumleleri_FlagYontemi.java
@@ -2,9 +2,10 @@ package day07_ifStatements_IfElseStatements;
 
 import java.util.Scanner;
 
-public class C02_BagimsizIfCumleleri_SayacYontemi {
+public class C03_BagimsizIfCumleleri_FlagYontemi {
 
     public static void main(String[] args) {
+
         // Kullanicidan iki tamsayi isteyin
 
         // girilen sayilara gore asagidaki degerlendirmeleri yazdirin
@@ -21,41 +22,36 @@ public class C02_BagimsizIfCumleleri_SayacYontemi {
         int sayi1 = scanner.nextInt();
         int sayi2 = scanner.nextInt();
 
-        int sayac = 0;
+        int flag = 10;
 
         // 1- sayi1 < sayi2 ise "birinci sayi daha kucuk"
         if (sayi1<sayi2){
             System.out.println("birinci sayi daha kucuk");
-            sayac++;
+            flag = 11;
         }
         // 2- sayi1 pozitif ise "birinci sayi sifirdan buyuk"
         if (sayi1>0){
             System.out.println("birinci sayi sifirdan buyuk");
-            sayac++;
+            flag = 11;
         }
         // 3- sayi2 50'den buyuk ise "ikinci sayi 50'den kucuk"
         if (sayi2>50){
             System.out.println("ikinci sayi 50'den buyuk");
-            sayac++;
+            flag = 11;
         }
         // 4- sayi2 5 ile bolunebiliyorsa "ikinci sayi 5'in tam kati"
         if (sayi2 % 5 == 0){
             System.out.println("ikinci sayi 5'in tam kati");
-            sayac++;
+            flag = 11;
         }
 
 
         // 5- EGER konsolda yukardakilerden hicbiri yazdirilmazsa
         //    "Nasil sayi sectiniz , hicbir sarti saglamiyor" yazdirin
 
-        if (sayac == 0) System.out.println("Nasil sayi sectiniz , hicbir sarti saglamiyor");
-
-
-        // 6- EGER tum sartlari sagliyorsa
-        //    "aferin, cok guzel sayi secmissin" yazdirin
-
-        if (sayac == 4) System.out.println("aferin, cok guzel sayi secmissin");
-
+        if (flag == 10){
+            System.out.println("Nasil sayi sectiniz , hicbir sarti saglamiyor");
+        }
 
     }
 }
