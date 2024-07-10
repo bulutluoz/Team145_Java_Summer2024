0fca9f1ed45bdf6aeb8ad923c2491919d409e04c
bulutluoz <abbulutluoz@gmail.com>
2024-07-10 20:19:55 +0200

    8.gun sonu

--- src/day08_ifElseIfStatements_NestedifElse/C04_EmekliOlabilirmiyim.java
+++ src/day08_ifElseIfStatements_NestedifElse/C04_EmekliOlabilirmiyim.java
@@ -10,6 +10,8 @@ public class C04_EmekliOlabilirmiyim {
         // Kadin, 60 yas ve uzeri , Erkek 65 yas ve uzeri emekli olabilir.
         // Cinsiyet ve yasini dikkate alarak “Emekli olabilirsin”
         // veya “Emekli olmak icin .. Yil daha calisman gerekir” yazdirin.
+        // kullanici E ve K disinda bir cinsiyet girerse
+        // veya 15 yasdan kucuk, 80 yasdan buyuk yas girerse uyarin
 
         Scanner scanner = new Scanner(System.in);
 
@@ -22,7 +24,17 @@ public class C04_EmekliOlabilirmiyim {
         System.out.println("Lutfen yasinizi giriniz...");
         double yas = scanner.nextDouble();
 
-
+        if (yas < 15 || yas >80  ||    ! ( cinsiyet == 'K' || cinsiyet=='E')     ){
+            System.out.println("Girilen degerler icin emeklilik hesaplanamaz");
+        } else if ( cinsiyet == 'K' && yas>= 60) {
+            System.out.println("kadin, 60 yasindan buyuk oldugundan emekli olabilir");
+        } else if (cinsiyet == 'K' && yas<60) {
+            System.out.println("Emeklilik yasi 60, daha "+ (60-yas) + " yil calismali");
+        } else if (cinsiyet == 'E' && yas >=65) {
+            System.out.println("erkek, 65 yasindan buyuk oldugundan emekli olabilir");
+        } else if (cinsiyet == 'E' && yas<65) {
+            System.out.println("Emeklilik yasi 65, daha "+ (65-yas) + " yil calismali");
+        }
 
     }
 }
---
+++ src/day08_ifElseIfStatements_NestedifElse/C05_IndirimliFiyatHesaplama.java
@@ -0,0 +1,16 @@
+package day08_ifElseIfStatements_NestedifElse;
+
+public class C05_IndirimliFiyatHesaplama {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan aldigi urun adedini ve indirimsiz fiyatini alin,
+        // kullaniciya musteri karti olup olmadigini sorun.
+        // Musteri karti varsa 10 urunden fazla alirsa %20,
+        //                        az alirsa %15 indirim yapin,
+        // Musteri karti yoksa 10 urunden fazla alirsa %15,
+        //                        az alirsa %10 indirim yapin
+
+
+    }
+}
