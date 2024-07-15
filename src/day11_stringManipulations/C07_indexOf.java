90919626905c13f802d4c3b63158f356bee60010
bulutluoz <abbulutluoz@gmail.com>
2024-07-15 20:37:15 +0200

    day11 sonu

---
+++ src/day11_stringManipulations/C07_indexOf.java
@@ -0,0 +1,25 @@
+package day11_stringManipulations;
+
+import java.util.Scanner;
+
+public class C07_indexOf {
+    public static void main(String[] args) {
+
+        // Kullanicidan bir cumle ve cumlede aratilacak bir metin isteyin
+        // girilen cumle ve metin'e gore asagidaki 3 sonucdan uygun olani yazdirin
+        // 1- cumle aranan metni icermiyor
+        // 2- cumle aranan metni sadece 1 adet iceriyor
+        // 3- cumle aranan metni 1'den fazla iceriyor
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen bir cumle giriniz...");
+        String cumle = scanner.nextLine();
+
+        System.out.println("Lutfen cumlede aranacak metni girin...");
+        String metin = scanner.nextLine();
+
+
+
+    }
+}
