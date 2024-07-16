ff3a04c9cf9a55347275d02aa982defe411855d1
bulutluoz <abbulutluoz@gmail.com>
2024-07-16 20:05:17 +0200

    day11 sonu

--- src/day12_stringManipulations/C03_ReplaceAll.java
+++ src/day12_stringManipulations/C03_ReplaceAll.java
@@ -5,10 +5,25 @@ public class C03_ReplaceAll {
 
         //Soru 2 : Kullanicinin belirli bir formatta verdigi String fiyatlari toplayip yazdirin.
         //		   input1 : “15.30 €” , input2 : “11.45 €”
-        //		   output : 26.70 €
+        //		   output : 26.75 €
 
         String fiyatStr1 = "15.30 €";
         String fiyatStr2 = "11.45 €";
+
+        System.out.println(  fiyatStr1 + fiyatStr2  ); //  15.30 €11.45 €
+
+        fiyatStr1 = fiyatStr1.replaceAll("\\D",""); // "1530"
+        fiyatStr2 = fiyatStr2.replaceAll("\\D",""); // "1145"
+
+        double fiyat1 = Double.parseDouble(fiyatStr1); // 1530.0
+        double fiyat2 = Double.parseDouble(fiyatStr2); // 1145.0
+
+        fiyat1 /= 100; // 15.30
+        fiyat2 /= 100; // 11.45
+
+        System.out.println(fiyat1 + fiyat2 +" €"); // "26.75 €"
+
+
 
 
     }
---
+++ src/day12_stringManipulations/C04_ReplaceAll.java
@@ -0,0 +1,16 @@
+package day12_stringManipulations;
+
+public class C04_ReplaceAll {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan ismini, soyismini ve Kredi karti numarasini alin
+        // isim ve soyismin ilk harfini buyuk harfe cevirip, kalanlari * yapin
+        // kredi karti numarasindaki sayilari * yapip asagidaki formatta yazdirin
+
+        // isminiz : A** Y*****
+        // Kart no : **** **** **** ****
+
+
+    }
+}
