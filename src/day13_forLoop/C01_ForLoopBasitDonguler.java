236f9867f6c15c282be14aa0245eee39c64d86c8
bulutluoz <abbulutluoz@gmail.com>
2024-07-17 19:49:51 +0200

    day13 sonu

---
+++ src/day13_forLoop/C01_ForLoopBasitDonguler.java
@@ -0,0 +1,16 @@
+package day13_forLoop;
+
+public class C01_ForLoopBasitDonguler {
+
+    public static void main(String[] args) {
+
+        // 1'den baslayip, 50'ye kadar (50 dahil) tek sayilari yazdirin
+
+        for (int i = 1; i <= 50 ; i+=2 ) {
+            System.out.print(i + " ");
+        }
+
+
+
+    }
+}
