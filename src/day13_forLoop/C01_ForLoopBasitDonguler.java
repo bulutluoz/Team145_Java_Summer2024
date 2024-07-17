a588c9aee24a6f1af0c2562cf034ed8a23eea130
bulutluoz <abbulutluoz@gmail.com>
2024-07-17 20:17:47 +0200

    day13 sonu

--- src/day13_forLoop/C01_ForLoopBasitDonguler.java
+++ src/day13_forLoop/C01_ForLoopBasitDonguler.java
@@ -10,7 +10,52 @@ public class C01_ForLoopBasitDonguler {
             System.out.print(i + " ");
         }
 
+        System.out.println("");
+        // 207'den baslayip 5'er artirarak, 400'den kucuk olan sayilari yazdirin
 
+        for (int i = 207; i <400 ; i+=5) {
+
+            System.out.print(i + " ");
+        }
+
+        System.out.println("");
+
+        // 2 basamakli cift sayilarin toplami kactir?
+
+        int toplam = 0;
+
+        for (int i = 10; i <100 ; i+=2) {
+
+            toplam += i ;
+        }
+
+        System.out.println("2 basamakli cift sayilarin toplami : " + toplam);
+
+
+        // 40 sayisini kalansiz bolebilen pozitif tamsayilari yazdirin
+        for (int i = 1; i <=40 ; i++) {
+
+            if ( 40 % i == 0){
+                System.out.print(i + " ");
+            }
+
+        }
+        System.out.println("");
+        // verilen baslangic ve bitis degerleri dahil olmak uzere
+        // bu sayilar arasinda 7 ile bolunebilen sayi adedini yazdirin
+
+        int baslangic = 235;
+        int bitis = 1673;
+        int sayac = 0 ;
+
+        for (int i = baslangic; i <=bitis ; i++) {
+
+            if ( i % 7 == 0){
+                sayac++;
+            }
+        }
+
+        System.out.println(baslangic + " ile "+ bitis + " arasinda " + sayac + " adet 7 ile bolunebilen sayi vardir");
 
     }
 }
---
+++ src/day13_forLoop/C02_DikkatEdilecekler.java
@@ -0,0 +1,4 @@
+package day13_forLoop;
+
+public class C02_DikkatEdilecekler {
+}
