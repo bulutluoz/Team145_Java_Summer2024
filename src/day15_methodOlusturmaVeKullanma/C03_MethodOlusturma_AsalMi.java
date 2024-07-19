49d77c8be75f951996b9658441f1a430fc416e5e
bulutluoz <abbulutluoz@gmail.com>
2024-07-19 20:53:14 +0200

    day15 sonu

--- src/day15_methodOlusturmaVeKullanma/C03_MethodOlusturma_AsalMi.java
+++ src/day15_methodOlusturmaVeKullanma/C03_MethodOlusturma_AsalMi.java
@@ -6,7 +6,24 @@ public class C03_MethodOlusturma_AsalMi {
 
         // verilen bir sayi asal ise true, asal degilse false yazdiran bir method olusturun
 
+        asalMi(82); // false
+        asalMi(97); // true
 
 
+
+
+    }
+
+    public static void asalMi(int sayi){
+        boolean asalMi = true;
+
+        for (int i = 2; i < sayi ; i++) {
+
+            if (sayi % i == 0){
+                asalMi = false;
+                break;
+            }
+        }
+        System.out.println(asalMi);
     }
 }
--- src/day15_methodOlusturmaVeKullanma/C07_MethodlariKullanma.java
+++ src/day15_methodOlusturmaVeKullanma/C07_MethodlariKullanma.java
@@ -10,6 +10,8 @@ public class C07_MethodlariKullanma {
         // 10! i yazdirin
         C02_MethodOlusturma.faktoryelDegeriYazdir(10); // 10! = 3628800
 
+        // 20719 asal ise true, asal degilse false yazdirin
+        C03_MethodOlusturma_AsalMi.asalMi(20719); // true
 
     }
 }
