ba25d095d674faf000a8c9d98d2eb9e0f4e9340c
bulutluoz <abbulutluoz@gmail.com>
2024-07-22 21:44:43 +0200

    day16 sonu

---
+++ src/day16_MethodKullanma_MethodOverloading/C09_WhileLoop.java
@@ -0,0 +1,20 @@
+package day16_MethodKullanma_MethodOverloading;
+
+import java.util.Scanner;
+
+public class C09_WhileLoop {
+
+    public static void main(String[] args) {
+
+        // kullanicidan istedigi kadar isim girmesini isteyin
+        // kullanici islemi bitirmek icin Q'ya basmalidir (q veya Q olabilir)
+        // girilen her ismin uzunlugunu kontrol edin ve
+        // islem bitirildiginde en cok harfe sahip olan (en uzun) ismi yazdirin
+        // esit sayida harfe sahip isimlerden herhangi biri olabilir
+
+        Scanner scanner = new Scanner(System.in);
+        String enUzunKelime = "";
+
+
+    }
+}
