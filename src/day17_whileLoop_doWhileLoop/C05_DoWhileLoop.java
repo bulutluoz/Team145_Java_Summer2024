563bf82a02b0159ea47072f0c4cfbf07e380c64f
bulutluoz <abbulutluoz@gmail.com>
2024-07-23 21:30:51 +0200

    day17 sonu

---
+++ src/day17_whileLoop_doWhileLoop/C05_DoWhileLoop.java
@@ -0,0 +1,41 @@
+package day17_whileLoop_doWhileLoop;
+
+import java.util.Scanner;
+
+public class C05_DoWhileLoop {
+
+    public static void main(String[] args) {
+
+        // kullanicidan toplanmak uzere sayilar isteyin
+        // kullanici 0'a bastiginda islemi bitirin
+        // ve girilen sayilar icerisinde en kucuk olan sayiyi yazdirin
+
+        Scanner scanner = new Scanner(System.in);
+        int sayi = 34;
+        /*
+            while loop'un en zayif tarafi
+            kullanicidan alacagimiz deger icin baslangicta bizim bir deger atamamizdir
+
+            Eger loop'u kullanicidan alacagimiz bir variable'a gore kurguladigimizda
+            bizim ilk deger atamasi yapmamiz gerekiyorsa
+            loop'un calismasini saglayacak bir deger ATAMALIYIZ
+         */
+        int enKucukSayi = Integer.MAX_VALUE; //
+
+
+        while ( sayi != 0){
+
+            System.out.println("Lutfen bir tam sayi giriniz..");
+            sayi = scanner.nextInt();
+
+            if ( sayi < enKucukSayi){
+
+                if (sayi != 0){
+                    enKucukSayi = sayi;
+                }
+            }
+        }
+
+        System.out.println("Girilen en kucuk sayi : " + enKucukSayi);
+    }
+}
