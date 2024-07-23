efa992fab969fc5a27d2a6ef029d070e1f95097a
bulutluoz <abbulutluoz@gmail.com>
2024-07-23 19:48:52 +0200

    day17 sonu

--- src/day16_MethodKullanma_MethodOverloading/C09_WhileLoop.java
+++ src/day16_MethodKullanma_MethodOverloading/C09_WhileLoop.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class C09_WhileLoop {
 
     public static void main(String[] args) {
+        // asagidaki gorevi yapan bir method olusturun :)
 
         // kullanicidan istedigi kadar isim girmesini isteyin
         // kullanici islemi bitirmek icin Q'ya basmalidir (q veya Q olabilir)
@@ -12,9 +13,50 @@ public class C09_WhileLoop {
         // islem bitirildiginde en cok harfe sahip olan (en uzun) ismi yazdirin
         // esit sayida harfe sahip isimlerden herhangi biri olabilir
 
+        enUzunKelimeyiBulVeYazdir();
+        System.out.println(enUzunKelimeyiBulVeDondur());
+
+    }
+
+
+    public static void enUzunKelimeyiBulVeYazdir(){
         Scanner scanner = new Scanner(System.in);
         String enUzunKelime = "";
+        String girilenKelime = "";
 
+        while ( !girilenKelime.equalsIgnoreCase("q")  ){
 
+            System.out.println("Lutfen bir kelime giriniz...\nBitirmek icin Q'ya basiniz..");
+            girilenKelime = scanner.nextLine();
+
+            if (girilenKelime.length() > enUzunKelime.length()){
+
+                enUzunKelime = girilenKelime;
+
+            }
+
+        }
+
+        System.out.println(enUzunKelime);
+    }
+
+    public static String enUzunKelimeyiBulVeDondur(){
+        Scanner scanner = new Scanner(System.in);
+        String enUzunKelime = "";
+        String girilenKelime = "";
+
+        while ( !girilenKelime.equalsIgnoreCase("q")  ){
+
+            System.out.println("Lutfen bir kelime giriniz...\nBitirmek icin Q'ya basiniz..");
+            girilenKelime = scanner.nextLine();
+
+            if (girilenKelime.length() > enUzunKelime.length()){
+
+                enUzunKelime = girilenKelime;
+
+            }
+
+        }
+        return enUzunKelime;
     }
 }
