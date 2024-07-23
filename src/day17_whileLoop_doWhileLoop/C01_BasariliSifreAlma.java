ed444892249b44e694a04c5b13a123a3cf5ffa14
bulutluoz <abbulutluoz@gmail.com>
2024-07-23 20:35:11 +0200

    day17 sonu

--- src/day17_whileLoop_doWhileLoop/C01_BasariliSifreAlma.java
+++ src/day17_whileLoop_doWhileLoop/C01_BasariliSifreAlma.java
@@ -1,5 +1,7 @@
 package day17_whileLoop_doWhileLoop;
 
+import java.util.Scanner;
+
 public class C01_BasariliSifreAlma {
 
     public static void main(String[] args) {
@@ -7,10 +9,29 @@ public class C01_BasariliSifreAlma {
         // day12 C08'de yaptigimiz sifre kontrolu sorusunu
         // sifre basarisiz oldugunda hatalari yazdirip, false donen
         // sifre basarili oldugunda ise true donen bir method haline getirin
+        //        System.out.println(sifreKontrolEt("Sa gd")); // false
+        //        System.out.println(sifreKontrolEt("agsgsjsjk90876")); // true
 
         // olusturdugunuz methodu kullanarak
         // kullanici hatasiz bir sifre girinceye kadar tekrar tekrar sifre isteyin
         // kabul edilebilir bir sifre girdiginde "Sifreniz basarili olarak kaydedildi" yazdirin
+
+        Scanner scanner = new Scanner(System.in);
+        String sifre = "";
+        boolean sifreUygunMu = false;
+
+        while ( !sifreUygunMu ){  // sifreUygunMu == false
+
+            System.out.println("Lutfen sifrenizi giriniz...");
+            sifre = scanner.nextLine(); // ali can ==> Alican1234 ==> aliCan1234
+
+            sifreUygunMu = sifreKontrolEt(sifre);
+
+        }
+
+        System.out.println("Sifreniz basarili olarak kaydedildi");
+
+
 
 
     }
@@ -20,36 +41,34 @@ public class C01_BasariliSifreAlma {
         int hataSayaci = 0;
 
         //  - ilk harf kucuk harf olmali
-
         if (  ! Character.isLowerCase(sifre.charAt(0))  ){
-
             System.out.println("Ilk karakter kucuk harf olmali");
             hataSayaci++;
         }
 
-
         //  - son karakter rakam olmali
-
         if ( ! Character.isDigit( sifre.charAt( sifre.length()-1)  )  ){
             System.out.println("Son karakter rakam olmali");
             hataSayaci++;
         }
 
-
         //  - sifre bosluk icermemeli
-
         if ( sifre.contains(" ")){
             System.out.println("Sifre bosluk barindiramaz");
             hataSayaci++;
         }
 
         //  - uzunlugu en az 10 karakter olmali
-
         if (sifre.length() < 10){
             System.out.println("sifrenin uzunlugu en az 10 karakter olmali");
             hataSayaci++;
         }
 
+        if (hataSayaci == 0 ){
+            return true;
+        }else {
+            return false;
+        }
 
     }
 }
---
+++ src/day17_whileLoop_doWhileLoop/C02_SayiToplama.java
@@ -0,0 +1,17 @@
+package day17_whileLoop_doWhileLoop;
+
+public class C02_SayiToplama {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan toplanmak uzere pozitif tamsayilar isteyin
+        // Kullanicinin girdigi pozitif sayilarin toplami 500 veya daha fazla oldugunda
+        // toplam kac adet pozitif tam sayi girdigini
+        // ve bunlarin toplaminin kac oldugunu yazdirin
+        // Kullanici 0 veya negatif sayi girerse "sayi pozitif olmalidir " yazdirin
+        // ve girilen uygun olmayan sayiyi sayi adedine ve toplama eklemeyin
+
+
+
+    }
+}
