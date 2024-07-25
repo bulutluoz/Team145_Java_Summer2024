aaa3eb842656a7eec35fc66cac23d89c436604d4
bulutluoz <abbulutluoz@gmail.com>
2024-07-25 21:54:00 +0200

    day18 sonu

---
+++ src/day19_arrays/C05_ArraysdehafizaKullanimi.java
@@ -0,0 +1,13 @@
+package day19_arrays;
+
+public class C05_ArraysdehafizaKullanimi {
+    public static void main(String[] args) {
+
+        String[] isimler = {"Sabri","Furkan","Cem"};
+
+        int[] sayilar = {3,5,6,7,8};
+
+
+
+    }
+}
