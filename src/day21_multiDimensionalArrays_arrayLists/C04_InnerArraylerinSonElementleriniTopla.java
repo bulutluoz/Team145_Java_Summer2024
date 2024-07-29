727059b042a8727f5528568453957c9ce96fdf2e
bulutluoz <abbulutluoz@gmail.com>
2024-07-29 20:51:02 +0200

    day21 sonu

--- src/day21_multiDimensionalArrays_arrayLists/C04_InnerArraylerinSonElementleriniTopla.java
+++ src/day21_multiDimensionalArrays_arrayLists/C04_InnerArraylerinSonElementleriniTopla.java
@@ -10,7 +10,11 @@ public class C04_InnerArraylerinSonElementleriniTopla {
 
         int[][] arr =  {{3,1,2,4},{1,2},{3,4,5},{10},{2,7}};
 
+        sonElementleriTopla(arr); // 28
 
+        int[][] arr2 =  {{3,1,2},{1},{3,4,5,7},{10,3},{2,7,1}};
+
+        sonElementleriTopla(arr2); // 14
 
     }
 
@@ -18,7 +22,13 @@ public class C04_InnerArraylerinSonElementleriniTopla {
 
         int toplam = 0 ;
 
+        for (int i = 0; i < arr.length ; i++) {
 
+            toplam += arr[i][ arr[i].length-1 ];
+
+        }
+
+        System.out.println(toplam);
 
     }
 }
---
+++ src/day21_multiDimensionalArrays_arrayLists/C05_AyniIndexdekiElementler.java
@@ -0,0 +1,15 @@
+package day21_multiDimensionalArrays_arrayLists;
+
+public class C05_AyniIndexdekiElementler {
+
+    public static void main(String[] args) {
+
+        // Verilen 2 katli bir array’de
+        // ayni index’e sahip elementleri toplayip,
+        // yeni olusturacagimiz tek katli bir array’e bu toplamlari atayin.
+        //		input :      int[][] arr =  {{3,4,5}, {2,3,6,7}};
+        //	  output:                       [5, 7, 11]
+
+
+    }
+}
