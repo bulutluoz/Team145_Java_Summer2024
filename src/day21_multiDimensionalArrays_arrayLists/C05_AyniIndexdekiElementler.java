c825ae2a1e5af04095f70aa69cab413f924f2e45
bulutluoz <abbulutluoz@gmail.com>
2024-07-29 21:12:27 +0200

    day21 sonu

--- src/day21_multiDimensionalArrays_arrayLists/C05_AyniIndexdekiElementler.java
+++ src/day21_multiDimensionalArrays_arrayLists/C05_AyniIndexdekiElementler.java
@@ -1,5 +1,7 @@
 package day21_multiDimensionalArrays_arrayLists;
 
+import java.util.Arrays;
+
 public class C05_AyniIndexdekiElementler {
 
     public static void main(String[] args) {
@@ -10,6 +12,19 @@ public class C05_AyniIndexdekiElementler {
         //		input :      int[][] arr =  {{3,4,5}, {2,3,6,7}};
         //	  output:                       [5, 7, 11]
 
+        int[][] arr =  {{3,4,5}, {2,3,6,7}};
+
+        int ortakIndexSayisi = arr[0].length < arr[1].length ? arr[0].length : arr[1].length ;
+
+        int[] ortakIndexArr = new int[ortakIndexSayisi]; // [0, 0, 0]
+
+
+        for (int i = 0; i <ortakIndexArr.length ; i++) {
+
+            ortakIndexArr[i] = arr[0][i] + arr[1][i];
+        }
+
+        System.out.println(Arrays.toString(ortakIndexArr)); // [5, 7, 11]
 
     }
 }
