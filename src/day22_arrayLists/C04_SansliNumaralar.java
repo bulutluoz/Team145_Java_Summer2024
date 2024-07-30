96dbea932b6ad20bf036125f103c034b63980f89
bulutluoz <abbulutluoz@gmail.com>
2024-07-30 20:19:59 +0200

    day22 sonu

--- src/day22_arrayLists/C04_SansliNumaralar.java
+++ src/day22_arrayLists/C04_SansliNumaralar.java
@@ -11,7 +11,45 @@ public class C04_SansliNumaralar {
         // calistirdigimizda 1milyondan kucuk sayilardan sansli 5 sayi secip
         // bir liste olarak bize donduren bir method olusturun
 
+        System.out.println(getSansliSayilar()); // [736996, 75931, 92890, 724199, 370469]
+        System.out.println(getSansliSayilar()); // [327189, 110707, 112078, 578976, 346462]
+        System.out.println(getSansliSayilar()); // [58861, 56416, 397135, 367822, 515233]
 
+
+        // verilen baslangic ve bitis derleri arasinda
+        // 5 tane sansli sayi secip
+        // bir liste olarak donduren bir method olusturun
+        // sayilar birbirinden farkli olmalidir
+
+        System.out.println(istenenAraliktanSanslisayiUret(200, 300));
+        // [270, 288, 279, 277, 250]
+
+        System.out.println(istenenAraliktanSanslisayiUret(3400, 4300));
+        // [3579, 3782, 3488, 3808, 3669]
+
+        System.out.println(istenenAraliktanSanslisayiUret(10, 16)); // [12, 14, 10, 13, 11]
+
+    }
+
+
+    public static List<Integer> istenenAraliktanSanslisayiUret(int baslangic, int bitis){
+
+        // ornek olarak 2200 ile 3000 arasinda 5 sayi uretsin
+        List<Integer> sansliSayilarList = new ArrayList<>();
+
+        Random random = new Random();
+
+        while (sansliSayilarList.size() < 5) {
+
+            int sayi = random.nextInt(bitis-baslangic);
+
+            if ( !sansliSayilarList.contains(sayi+baslangic)){
+                sansliSayilarList.add( sayi+baslangic );
+            }
+
+        }
+
+        return sansliSayilarList;
     }
 
     public static List<Integer> getSansliSayilar( ){
@@ -19,7 +57,12 @@ public class C04_SansliNumaralar {
         List<Integer> sansliSayilarList = new ArrayList<>();
 
         Random random = new Random();
-        int sayi = random.nextInt(1000000);
+
+        for (int i = 0; i <5 ; i++) {
+
+            int sayi = random.nextInt(1000000);
+            sansliSayilarList.add(sayi);
+        }
 
         return sansliSayilarList;
     }
---
+++ src/day22_arrayLists/C05_Odev.java
@@ -0,0 +1,17 @@
+package day22_arrayLists;
+
+public class C05_Odev {
+
+    // Bir oyun method'u olusturun
+    // method 3 basamakli bir pozitif sayi uretip
+    // kullanicinin bu sayiyi tahmin etmesini isteyecektir
+    // kullanicinin tahmini uretilen sayidan kucukse kullaniciya "daha buyuk sayi girmelisin"
+    // kullanicinin tahmini uretilen sayidan buyukse kullaniciya "daha kucuk sayi girmelisin"
+    // yazdirarak, kullaniciya yol gosterin
+    // kullanici sayiyi buldugunda
+    // tahmin olarak girdigi tum sayilari ve kac tahminde sayiyi bulabildigini yazdirin
+    // tahmin sayisi <= 2 ise "Cok Sanslisin"
+    // tahmin sayisi 2 ile <=5 arasinda ise supersin
+    // 5 ile <=10 ise idare eder
+    // 10'dan buyukse daha zekice tahminler yapmalisin yazdirin
+}
