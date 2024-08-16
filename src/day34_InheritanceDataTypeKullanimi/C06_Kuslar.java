c4fe797b30f9b837c4c327d52c8680645bf4cc2a
bulutluoz <abbulutluoz@gmail.com>
2024-08-16 21:39:07 +0200

    day34 sonu

---
+++ src/day34_InheritanceDataTypeKullanimi/C06_Kuslar.java
@@ -0,0 +1,24 @@
+package day34_InheritanceDataTypeKullanimi;
+
+public class C06_Kuslar extends C05_Hayvanlar {
+    protected void kanat(){
+
+        System.out.println("kanatlidirlar");
+    }
+
+    protected void solunum(){
+
+        System.out.println("akcigerle nefes alirlar");
+    }
+
+    protected void gaga(){
+
+        System.out.println("gagalari vardir");
+    }
+
+    protected void cogalma(){
+
+        System.out.println("yumurtayla cogalirlar");
+    }
+
+}
---
+++ src/day34_InheritanceDataTypeKullanimi/C07_AvciKuslar.java
@@ -0,0 +1,20 @@
+package day34_InheritanceDataTypeKullanimi;
+
+public class C07_AvciKuslar extends C06_Kuslar{
+
+    public void hareket() {
+        System.out.println("ucarlar");
+    }
+
+    public void beslenme() {
+        System.out.println("et yerler");
+    }
+
+    public void pence() {
+        System.out.println("pencelidir");
+    }
+
+    public void gaga() {
+        System.out.println("sivri gagali");
+    }
+}
