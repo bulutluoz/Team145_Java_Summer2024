64523087e3ea4bc5bfa146cae502fe2666759c14
bulutluoz <abbulutluoz@gmail.com>
2024-08-16 19:52:12 +0200

    day34 sonu

---
+++ src/day34_InheritanceDataTypeKullanimi/C01_DataTypeKullanimindaFarklilik.java
@@ -0,0 +1,34 @@
+package day34_InheritanceDataTypeKullanimi;
+
+public class C01_DataTypeKullanimindaFarklilik {
+
+    /*
+        Hayvallarla ilgili semada
+
+        hem hayvanlar class'inda, hem kuslar class'inda, hem de ucan kuslar class'inda kartal var
+
+        her class'daki kartal ayni klartal olmasina ragmen
+        ozellikleri farkli olabilir
+
+        CUNKU cumlemize her zaman
+        Hayvanlar class'indaki kartal da , tum hayvanlar gibi... ile baslariz
+
+        bu durumda
+        Hayvanlar class'indaki kartal da , tum hayvanlar gibi...
+        Kuslar class'indaki kartal da , tum kuslar gibi...
+        AvciKuslar class'indaki kartal da , tum AvciKuslar gibi... cumleleri bize farkli sonuclar uretebilir
+
+        Bu konuda gercek dunyamizda 2 farkli durum mumkundur,
+        Java bu iki durumu da realize etmistir
+
+        1- Obje hangi class'dan olusturulmussa
+           o class'daki tum objelerle ortak ozelliklere sahip olur
+           (ozellik variable olarak olusturulursa)
+
+        2- Obje tabii ki olusturuldugu class'daki tum objelerle
+           baslangic itibariyle ortak ozellikleri tasir
+           AMMA semada daha guncel bilgi varsa onu kullanmalidir
+           (ozellik method olarak olusturulursa)
+
+     */
+}
