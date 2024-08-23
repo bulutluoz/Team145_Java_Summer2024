97ced7d59bde9ceef9fc55261ebba9002ce2432b
bulutluoz <abbulutluoz@gmail.com>
2024-08-23 21:55:01 +0200

    day39 sonu

---
+++ src/day39_exceptions/C05_TryCatchCalismaMantigi.java
@@ -0,0 +1,39 @@
+package day39_exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class C05_TryCatchCalismaMantigi {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan 3 sayi alip
+        // toplamini yazdirin
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("1.sayiyi giriniz..");
+            double sayi1 = scanner.nextDouble();
+            System.out.println("1.sayi sorunsuz alinip kaydedildi");
+
+
+            System.out.println("2.sayiyi giriniz..");
+            double sayi2 = scanner.nextDouble();
+            System.out.println("2.sayi sorunsuz alinip kaydedildi");
+
+
+            System.out.println("3.sayiyi giriniz..");
+            double sayi3 = scanner.nextDouble();
+            System.out.println("3.sayi sorunsuz alinip kaydedildi");
+
+            System.out.println("Sayilarin toplami : " + (sayi1+sayi2+sayi3));
+
+
+        } catch (InputMismatchException e) {
+
+            System.out.println("Sayi girmeliydiniz...");
+        }
+
+    }
+}
