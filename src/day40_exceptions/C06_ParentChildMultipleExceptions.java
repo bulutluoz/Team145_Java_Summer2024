304f285ed0babc418f0bf65b9b94ec61e1c50514
bulutluoz <abbulutluoz@gmail.com>
2024-08-26 20:51:48 +0200

    day40 sonu

--- src/day40_exceptions/C06_ParentChildMultipleExceptions.java
+++ src/day40_exceptions/C06_ParentChildMultipleExceptions.java
@@ -1,9 +1,29 @@
 package day40_exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class C06_ParentChildMultipleExceptions {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+
+        // Notlar.txt dosyasindaki notlari yazdiralim
+
+        String dosyaYolu = "src/day40_exceptions/Notlar.txt";
+
+        // Notlar dosyasini yazdirabilmek icin once Javanin o dosyaya ulasmasi lazim
+
+        FileInputStream fileInputStream = new FileInputStream(dosyaYolu);
+        // Unhandled exception: java.io.FileNotFoundException
+
+        int k='a';
+
+        while((k= fileInputStream.read() ) != (-1)) { // dosyayi okuyup, bize getirme
+            // Unhandled exception: java.io.IOException
+            System.out.print((char) k);
+        }
 
     }
 }
